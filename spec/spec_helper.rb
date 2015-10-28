73ccbe23fd2e861e22a7ff619b820859f1fac264
jeffli <jeff.chun.yinli@gmail.com>
2015-10-28 11:26:53 -0400

    [jl gc] added focus true option when running rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,9 @@ require 'aws-sdk'
 require 'pry'
 require 'fuubar'
 require 'dynamini'
+
+RSpec.configure do |config|
+  # For running just wanted tests in guard
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end

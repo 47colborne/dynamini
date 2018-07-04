1404f2bfa5f18394204ed3bf62d4590b7bc2a44b
makeready <shotsinthedark@gmail.com>
2018-07-04 10:23:09 -0400

    added tests for ItemSplitter

--- lib/dynamini/item_splitter.rb
+++ lib/dynamini/item_splitter.rb
@@ -16,14 +16,14 @@ module Dynamini
           if size > MAX_SIZE
             part_one, part_two = split_au(unprocessed_au[0])
             unprocessed_au.shift
-            unprocessed_au.unshift(part_one)
             unprocessed_au.unshift(part_two)
+            unprocessed_au.unshift(part_one)
           else
             current_update_size += size
             if current_update_size > MAX_SIZE
               updates.push(current_update)
               current_update_size = 0
-              current_update = []
+              current_update = {}
             else
               current_update_size += size
               key, value = key_and_value(unprocessed_au[0])
@@ -44,14 +44,13 @@ module Dynamini
       end
 
       def split_au(au)
-        # input: {"sec"=>{:action=>"PUT", :value=>"[1,2,3]"}
         attribute_name = au.keys[0]
         attribute_action = au.values[0][:action]
         attribute_value = au.values[0][:value]
 
-        raise "#{attribute_name} is not enumerable and is too large to save." unless attribute_value.is_a?(Enumerable)
+        raise "#{attribute_name} is too large to save and is not splittable (not enumerable)." unless attribute_value.is_a?(Enumerable)
 
-        part_one = {attribute_name => {action: attribute_action, value: attribute_value[0..attribute_value.length / 2]}}
+        part_one = {attribute_name => {action: attribute_action, value: attribute_value[0..(attribute_value.length / 2) - 1]}}
         part_two = {attribute_name => {action: "ADD", value: attribute_value[attribute_value.length / 2..-1]}}
         [part_one, part_two]
       end
--- spec/dynamini/item_splitter_spec.rb
+++ spec/dynamini/item_splitter_spec.rb
@@ -1,7 +1,40 @@
 require 'spec_helper'
 
 describe Dynamini::ItemSplitter do
-  it('does') do
-    true
+  before do
+    stub_const("Dynamini::ItemSplitter::MAX_SIZE", 200)
+  end
+
+  context 'regular update' do
+    it 'returns one update' do
+      input = {foo: {action: 'PUT', value: '123'}, bar: {action: 'ADD', value: [4,5,6]}}
+      expect(Dynamini::ItemSplitter.split(input)).to eq([input])
+    end
+  end
+
+  context 'large update' do
+    it 'returns multiple updates' do
+      input = {foo: {action: 'PUT', value: '123'}, bar: {action: 'ADD', value: [4,5,6]}, baz: {action: 'PUT', value: 'hello'}}
+      output = Dynamini::ItemSplitter.split(input)
+      expect(output[0]).to eq({foo: {action: 'PUT', value: '123'}, bar: {action: 'ADD', value: [4,5,6]}})
+      expect(output[1]).to eq(baz: {action: 'PUT', value: 'hello'})
+    end
+  end
+
+  context 'large enumerable attribute' do
+    it 'splits the attribute' do
+      input = {enum: {action: 'PUT', value: Array.new(20, 'hello')}}
+      output = Dynamini::ItemSplitter.split(input)
+      expect(output.length).to eq(2)
+      expect(output[0][:enum][:action]).to eq('PUT')
+      expect(output[1][:enum][:action]).to eq('ADD')
+    end
+  end
+
+  context 'large non-enumerable attribute' do
+    it 'raises an error' do
+      input = {not_enum: {action: 'PUT', value: Array.new(20, 'hello').to_s}}
+      expect{ Dynamini::ItemSplitter.split(input) }.to raise_error("not_enum is too large to save and is not splittable (not enumerable).")
+    end
   end
 end
